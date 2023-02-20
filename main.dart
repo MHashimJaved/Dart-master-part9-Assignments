ea851420a459d1255ed3719523e74bb612a5e5c8
Muhammad Hashim Javed <53869097+MHashimJaved@users.noreply.github.com>
2023-02-20 13:26:59 +0500

    create main.dart
    Rules of declaration

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main() {
+  //rules of declaration:
+  
+  int numberOne; //this is how we declare the variable
+  int numberTwo;  // this is how we initialize the variable 
+  int sum;
+  
+  numberOne = 30;
+  numberTwo = 50;
+  
+  sum = numberOne + numberTwo;
+  print(sum);
+  
+}
