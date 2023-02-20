542159186feaf9729ec1cf7ba9280d7970b3fc21
Muhammad Hashim Javed <53869097+MHashimJaved@users.noreply.github.com>
2023-02-20 13:54:57 +0500

    Create main2.dart
    arthematice operators: + , - , *

---
+++ main2.dart
@@ -0,0 +1,25 @@
+void main() {
+  //rules of declaring vairable:
+//arthematice operators: + , - , *
+  
+  int numberOne; //this is how we declare the variable
+  int numberTwo;  // this is how we initialize the variable 
+  int sum;
+  int subtractResult;
+  int multiplyResult;
+  double divide;
+  
+  numberOne = 30;
+  numberTwo = 50;
+  
+  sum = numberOne + numberTwo;
+  subtractResult = numberOne - numberTwo;
+  multiplyResult = numberOne * numberTwo;
+  divide = numberOne / numberTwo;
+  
+  print(sum);
+  print(subtractResult);
+  print(multiplyResult);
+  print(divide);
+  
+}
