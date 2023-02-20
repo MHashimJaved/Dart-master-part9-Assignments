54f6cb013b588ca1d118bf6053008a08dc50f4a0
Muhammad Hashim Javed <53869097+MHashimJaved@users.noreply.github.com>
2023-02-20 13:59:20 +0500

    Create main3.dart
     // double variable concept to show in decimal value

---
+++ main3.dart
@@ -0,0 +1,26 @@
+void main() {
+  //rules of declaring vairable:
+//arthematice operators: + , - , *, /
+  // double variable concept to show in decimal value
+  
+  double numberOne; //this is how we declare the variable
+  double numberTwo;  // this is how we initialize the variable 
+  double sum;
+  double subtractResult;
+  double multiplyResult;
+  double divide;
+  
+  numberOne = 30;
+  numberTwo = 50;
+  
+  sum = numberOne + numberTwo;
+  subtractResult = numberOne - numberTwo;
+  multiplyResult = numberOne * numberTwo;
+  divide = numberOne / numberTwo;
+  
+  print(sum);
+  print(subtractResult);
+  print(multiplyResult);
+  print(divide);
+  
+}
