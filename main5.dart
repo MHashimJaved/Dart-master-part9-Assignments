64d8467d5d6549fb7dc2ac0d9a56751c7ddd2eca
Muhammad Hashim Javed <53869097+MHashimJaved@users.noreply.github.com>
2023-02-21 15:11:44 +0500

    Create main5.dart
    Swap values of two variables into variable x and y

---
+++ main5.dart
@@ -0,0 +1,12 @@
+void main() {
+  //Swap values of two variables:
+  int x;
+  x = 10;
+  int y;
+  y = 20;
+  
+  print('before swaping');
+  print('value of x: $x');
+  print('value of y: $y');
+  
+}
