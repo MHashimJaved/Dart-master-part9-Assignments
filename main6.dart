847a1dd034c5c02b65c91aa4b21f4225084a6db4
Muhammad Hashim Javed <53869097+MHashimJaved@users.noreply.github.com>
2023-02-22 19:41:27 +0500

    Create main6.dart

---
+++ main6.dart
@@ -0,0 +1,17 @@
+void main() {
+  //Swap values of two variables:
+  //Swap value after swaping
+  int x;
+  x = 10;
+  int y;
+  y = 20;
+  
+  print('before swaping');
+  print('value of x: $x');
+  print('value of y: $y');
+  
+  print('after swaping');
+  print('value of x: $x');
+  print('value of y: $y');
+  
+}
