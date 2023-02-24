b17af2546e3c7d27f36694496e808a383a510bba
Muhammad Hashim Javed <53869097+MHashimJaved@users.noreply.github.com>
2023-02-24 18:33:57 +0500

    Create main7.dart
    Swapping after than before and adding conversational digits

---
+++ main7.dart
@@ -0,0 +1,20 @@
+void main() {
+  int x;
+  x = 10;
+  int y;
+  y = 20;
+                               //Before swapping 
+                               //After swaping
+  print('before swaping');
+  print('value of x: $x');
+  print('value of y: $y');
+  
+  x = x + y;
+  
+  print('after swaping');
+  print('value of x: $x');
+  print('value of y: $y');
+ 
+}
+
+
