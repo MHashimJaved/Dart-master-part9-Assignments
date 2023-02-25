55fc4a08fec9f4bf95793c9be5d28612a434aadb
Muhammad Hashim Javed <53869097+MHashimJaved@users.noreply.github.com>
2023-02-25 15:38:06 +0500

    Create  main8.dart
    //Write Program to convert feet to metres and metres into KM.

---
+++ main8.dart
@@ -0,0 +1,14 @@
+void main () {
+  double feet = 2000;
+  //Write Program to convert feet to metres and metres into KM.
+  double meter, km;
+  
+  meter = feet/3;
+  
+  km = meter/1000;
+  
+    print ('meter: $meter');
+    print ('Kilometer: $km');
+  
+  
+    }
