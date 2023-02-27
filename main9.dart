fee2be3de5a0c5e03638537ed5db50434ba4d4d5
Muhammad Hashim Javed <53869097+MHashimJaved@users.noreply.github.com>
2023-02-27 19:35:15 +0500

    Create main9.dart
    What is const keyword ?

---
+++ main9.dart
@@ -0,0 +1,15 @@
+void main() {
+  
+  
+  const double pi = 3.14;
+ 
+  double temp = 25.3 , volume = 20.5;
+  int age = 23, marks = 100;
+  
+  print(pi);
+  print(age);
+  print(marks);
+  print(temp);
+  print(volume);
+  
+}
